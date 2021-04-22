ff4ec5d96be908d57c81d105729bf3a9fbd00475
vietphan-billidentity <81551316+vietphan-billidentity@users.noreply.github.com>
2021-04-22 14:55:28 +1000

    Update script.groovy

--- groovy/script.groovy
+++ groovy/script.groovy
@@ -10,6 +10,7 @@ def deployApp() {
 	echo 'Call from groovy - deploying the application...'
     //echo "deploying version ${params.VERSION}"
     echo "${params.serverList}"
+    echo "length(${params.serverList})"
 }
 
 return this
