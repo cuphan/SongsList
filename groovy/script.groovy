265c893906819e0e325d5b7af770802794fb5d1e
vietphan-billidentity <81551316+vietphan-billidentity@users.noreply.github.com>
2021-04-22 15:18:41 +1000

    Update script.groovy

--- groovy/script.groovy
+++ groovy/script.groovy
@@ -10,7 +10,7 @@ def deployApp() {
     echo 'Call from groovy - deploying the application...'
     //echo "${params.serverList}"
     //echo "${length(params.serverList)}"
-   def lines = new String(params.serverList).split('\n')
+   def lines = new String(params.serverList).split(',')
    println("Server list: " + lines.size())
    for(int i = 0; i < lines.size(); i++) {
       println("ssh to " + lines[i].trim())
